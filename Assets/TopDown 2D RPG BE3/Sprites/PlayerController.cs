b0c5c231460f271fc40b213112c734e7c427d466
Richard De Sa Jr. <rdesa1@umassd.edu>
2025-03-02 16:12:23 -0500

    TODO: abstract synchronization logic

--- Assets/TopDown 2D RPG BE3/Sprites/PlayerController.cs
+++ Assets/TopDown 2D RPG BE3/Sprites/PlayerController.cs
@@ -1,5 +1,28 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Intervals
+{
+     [SerializeField] private float steps;
+     [SerializeField] private UnityEvent trigger;
+     private int lastInterval;
+
+     public float GetIntervalLength(float bpm)
+     {
+          return 60f / (bpm * steps);
+     }
+
+     public void CheckForNewInterval (float interval)
+     {
+          if (Mathf.FloorToInt(interval) != lastInterval)
+          {
+               lastInterval = Mathf.FloorToInt(interval);
+               trigger.Invoke();
+          }
+     }
+}
 
 public class PlayerController2D : MonoBehaviour
 {
@@ -19,9 +42,15 @@ public class PlayerController2D : MonoBehaviour
      public List<Sprite> wSprites;
 
      Vector2 direction; // Stores the direction of player movement
+     
+     
 
      // Private variables 
      private bool isMovingHorizontally = true; // Flag to track if the player is moving horizontally
+
+     [SerializeField] private float bpm;
+     [SerializeField] private AudioSource music; // Handles synchronization of the music
+     [SerializeField] private Intervals[] intervals;
 
      void Start()
      {
@@ -30,23 +59,20 @@ public class PlayerController2D : MonoBehaviour
 
      void Update()
      {
+          Synchronize(music);
+
           // get direction of input
           direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
           // set walk based on direction
           body.linearVelocity = direction * walkSpeed;
 
-          
           // get sprite that faces same direction as input
           List<Sprite> directionSprites =  GetSpriteDirection();
-
           if (directionSprites != null )
           {
                spriteRenderer.sprite = directionSprites[0];
-          } else
-          {
-
-          }
+          } 
      } 
 
      // determine the direction the character should face based on directional input
@@ -74,4 +100,15 @@ public class PlayerController2D : MonoBehaviour
           return selectedSprites;
      }
      
+     void Synchronize(AudioSource music)
+     {
+          foreach (Intervals interval in intervals)
+          {
+               float sampledTime = (music.timeSamples / music.clip.frequency * interval.GetIntervalLength(bpm));
+               interval.CheckForNewInterval(sampledTime);
+
+          }
+
+     }
+
 }
