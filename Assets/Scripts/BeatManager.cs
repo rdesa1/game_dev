e4748847a0d3c0bd26fbe68b9f645e78a33d07b4
Richard De Sa Jr. <rdesa1@umassd.edu>
2025-03-14 14:23:22 -0400

    bryan's suggestions

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -1,33 +1,51 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using System;
 
 public class BeatManager : MonoBehaviour
 {
-
-     // Private variables
      [SerializeField] private float bpm;
-     [SerializeField] private AudioSource music; // Handles synchronization of the music
+     [SerializeField] private AudioSource music;
      [SerializeField] private Intervals[] intervals;
 
-     // Start is called once before the first execution of Update after the MonoBehaviour is created
-     void Start()
+     // Event that passes beat timestamp
+     public static event Action<float> OnBeat;
+
+     // Window size in seconds within which a keypress is considered "on beat"
+     [SerializeField] private float beatWindowSize = 0.15f; // 150ms window
+
+     public static float LastBeatTime { get; private set; }
+     public static float BeatWindowSize { get; private set; }
+
+     void Awake()
      {
-        
+          BeatWindowSize = beatWindowSize;
      }
 
-     // Update is called once per frame
      void Update()
      {
           foreach (Intervals interval in intervals)
           {
                float sampledTime = (music.timeSamples / (music.clip.frequency * interval.GetIntervalLength(bpm)));
-               interval.CheckForNewInterval(sampledTime);
-               
+               bool newBeat = interval.CheckForNewInterval(sampledTime);
+
+               if (newBeat)
+               {
+                    // Store the beat time and broadcast the event
+                    LastBeatTime = Time.time;
+                    OnBeat?.Invoke(LastBeatTime);
+               }
           }
      }
-}
 
+     // Helper method to check if a timestamp was on beat
+     public static bool WasOnBeat(float inputTime)
+     {
+          // Check if input happened within the beat window
+          return Mathf.Abs(inputTime - LastBeatTime) <= BeatWindowSize;
+     }
+}
 
 [System.Serializable]
 public class Intervals
@@ -41,13 +59,14 @@ public class Intervals
           return 60f / (bpm * steps);
      }
 
-     public void CheckForNewInterval(float interval)
+     public bool CheckForNewInterval(float interval)
      {
           if (Mathf.FloorToInt(interval) != lastInterval)
           {
                lastInterval = Mathf.FloorToInt(interval);
                trigger.Invoke();
-               //Debug.Log("This is a beat");
+               return true;
           }
+          return false;
      }
 }
