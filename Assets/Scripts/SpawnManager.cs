747592bb114996161b58027ef465034c73e3c393
Richard De Sa Jr. <rdesa1@umassd.edu>
2025-03-21 00:31:00 -0400

    SpawnManager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -25,7 +25,7 @@ public class SpawnManager : MonoBehaviour
      // Start is called once before the first execution of Update after the MonoBehaviour is created
      void Start()
      {
-  
+
      }
 
      private void OnDisable()
